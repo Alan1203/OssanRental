e11d5d1bffc79be500ce2b40ebb1635feac01736
Alan1203 <alan841203@gmail.com>
2019-06-20 13:43:38 +0800

    debug2

--- OssanRentalMVC/OssanRentalMVC/src/main/java/com/web/store/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.web.store.service;
-
-
-import com.web.store.model.OssanBean;
-// 定義進行登入時系統必須執行的功能
-public interface LoginService {
-	public OssanBean checkIDPassword(String ossanId, String password) ;
-}
